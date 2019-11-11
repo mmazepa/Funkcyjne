4e1b6eccd8e6f9320c75193efabd8d52c6d5ece8
mmazepa <maksymilianmariusz@gmail.com>
2019-11-12 00:32:50 +0100

    Korekta skryptów.

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -1,1 +1,8 @@
-ghc -o $1 "$1".hs
+#!/bin/bash
+if [ ! -z $1 ]; then
+  ghc -o $1 "$1".hs
+  rm *.hi
+  rm *.o
+else
+  echo "[UWAGA]: Nie podano pliku do kompilacji!"
+fi
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,1 +1,6 @@
-./$1
+#!/bin/bash
+if [ ! -z $1 ]; then
+  ./$1
+else
+  echo "[UWAGA]: Nie podano pliku do uruchomienia!"
+fi
